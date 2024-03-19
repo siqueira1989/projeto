f36ba13d397efc44d3ac6d9606310bd2fb7b25d3
siqueira1989 <luciano.apsiqueira@gmail.com>
2024-03-19 10:02:30 -0300

    Alteração19/03

---
+++ index/Banco.sql
@@ -0,0 +1,8 @@
+Create TABLE colaboradores( 
+    id_colaboradores int AUTO_INCREMENT PRIMARY key,
+     nome_colaboradores varchar(100), 
+     senha_colaboradores varchar(50), 
+     funcao_colaboradores varchar(10), 
+     dataadmissao_colaboradores date, 
+situacao_colaboradores boolean, 
+nivel_colaboradores int);
